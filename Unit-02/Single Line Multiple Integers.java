b25e86e34e4540a2ecdd54087b3f83880103a24f
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-04-28 09:54:49 +0530

    Create Single Line Multiple Integers.java

---
+++ Unit-02/Single Line Multiple Integers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Main{
+    
+  public static void main(String [] args){
+       
+       Scanner sc = new Scanner(System.in);
+       
+     for(int i = 0; i<4; i++) {
+         
+         int num = sc.nextInt();
+    
+     
+     System.out.println(num+1);
+  } 
+  
+}
+
+}
