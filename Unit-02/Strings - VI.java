879f9ab93d174127443aeecd0b513722e28f919e
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-04-28 10:07:03 +0530

    Create Strings - VI.java

---
+++ Unit-02/Strings - VI.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+                sc.nextLine();
+         for( int i =0; i<t; i++){
+             String str = sc.nextLine();
+             System.out.println(str);
+         }
+    }
+}
