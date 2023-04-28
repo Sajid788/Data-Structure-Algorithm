3983a24bde6c691332e54e4e0b503531dd6e190a
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-04-28 10:02:30 +0530

    Create Strings - I.java

---
+++ Unit-02/Strings - I.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Main{
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = sc.next();
+        System.out.print(str);
+    }
+}
