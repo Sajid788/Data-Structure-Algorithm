67719113620258fffe3c3ca90a69f4f13291d0a4
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-04-29 10:38:16 +0530

    Create Day of the Week.java

---
+++ Unit-02/Key - Value Pairs/Day of the Week.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String day;
+
+        switch (n) {
+            case 0:
+                day = "Sunday";
+                break;
+            case 1:
+                day = "Monday";
+                break;
+            case 2:
+                day = "Tuesday";
+                break;
+            case 3:
+                day = "Wednesday";
+                break;
+            case 4:
+                day = "Thursday";
+                break;
+            case 5:
+                day = "Friday";
+                break;
+            case 6:
+                day = "Saturday";
+                break;
+            default:
+                day = "Invalid day number";
+                break;
+
+        }
+
+        System.out.println(day);
+    }
+}
+   
