ec62b0602c6a0f53a323cddc2f3165830bc08c5d
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-04-29 10:36:02 +0530

    Create Pattern Printing II.java

---
+++ Unit-02/Key - Value Pairs/Pattern Printing II.java
@@ -0,0 +1,14 @@
+public static void patternPrinting(int n){
+      
+    for (int i = 0; i<n; i++){
+        
+        for(int j=0; j<n; j++){
+            if(j==0 || j==n-1 || i==n-1){
+                System.out.print("* ");
+            }else{
+               System.out.print("  ");
+            }
+       }
+       System.out.println(); 
+    }
+  }
