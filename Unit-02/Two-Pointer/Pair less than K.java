a33a032ca67e183e4a56a328b3a00b938e149a83
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-05-06 16:26:43 +0530

    Create Pair less than K.java

---
+++ Unit-02/Two-Pointer/Pair less than K.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            Arrays.sort(arr);
+
+            int left = 0;
+            int right = n - 1;
+            int ans = -1;
+
+            while (right > left) {
+                int sum = arr[left] + arr[right];
+                if (sum < k) {
+                    ans = Math.max(ans, sum);
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+            System.out.println(ans);
+        }
+    }
+}
