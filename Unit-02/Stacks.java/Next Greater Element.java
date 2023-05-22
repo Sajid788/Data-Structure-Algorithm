c0ac28466435549927741774b6f95b4f743a07ed
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-05-22 19:22:59 +0530

    Create Next Greater Element.java

---
+++ Unit-02/Stacks.java/Next Greater Element.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- != 0) {
+            int num = sc.nextInt();
+            int[] arr = new int[num];
+            for (int i = 0; i < num; i++)
+                arr[i] = sc.nextInt();
+
+            int[] ans = new int[num];
+            Stack<Integer> st = new Stack<>();
+
+            for (int i = num - 1; i >= 0; i--) {
+                while (!st.isEmpty() && st.peek() <= arr[i])
+                    st.pop();
+                if (st.isEmpty())
+                    ans[i] = -1;
+                else
+                    ans[i] = st.peek();
+                st.push(arr[i]);
+            }
+
+            for (int i = 0; i < num; i++)
+                System.out.print(ans[i] + " ");
+            System.out.println();
+        }
+    }
+}
