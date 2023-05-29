d0988ac620b875f8d57fc7b26931d7c9406d3149
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2023-05-29 15:27:33 +0530

    Create Manage stacks of bricks.java

---
+++ Unit-02/DSA Contest [Week - 4]/Manage stacks of bricks.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Main{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    int t=sc.nextInt();
+    while(t--!=0) {
+      int n=sc.nextInt();
+      int x=sc.nextInt();
+      int y=sc.nextInt();
+      int[] ai=new int[n];
+      int[] bi=new int[n];
+      
+      for(int i=0; i<n; i++){
+        ai[i]=sc.nextInt();
+        bi[i]=sc.nextInt();
+      }
+      Arrays.sort(ai);
+      Arrays.sort(bi);
+      int effort = 0;
+      for(int i=0; i<n; i++){
+        if(bi[i]>ai[i]) {
+          effort += (bi[i] - ai[i])*x;
+        }
+        else if (bi[i]<ai[i]) {
+          effort += (ai[i] - bi[i])*y;
+        }
+      }
+      System.out.println(effort);
+    }   
+  }
+}
