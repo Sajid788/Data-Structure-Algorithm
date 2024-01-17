200876ad87a2469a01251eaafd88a9f2eb15b858
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2024-01-17 20:55:31 +0530

    Create Update & Compare.java

---
+++ Unit - 1/Conditional Statements and Logical Operator/Update & Compare.java
@@ -0,0 +1,12 @@
+  public static void updateCompare(int num1,int num2,int num3){
+     if (num1>num2){
+        System.out.println(true);
+    }else{
+        System.out.println(false);
+    }
+    if (num1+num3>num2){
+        System.out.println(true);
+    }else{
+        System.out.println(false);
+    }
+  }
