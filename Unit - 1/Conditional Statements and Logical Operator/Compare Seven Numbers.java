d8f7dce379a1013186da68fb752ef1929e606287
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2024-01-17 20:59:49 +0530

    Create Compare Seven Numbers.java

---
+++ Unit - 1/Conditional Statements and Logical Operator/Compare Seven Numbers.java
@@ -0,0 +1,10 @@
+public static void compareSevenNumbers(int a,int b,int c,int d,int e,int f,int g){
+      int sum1 = (a+b)*c;
+      int sum2 = (d+e+f+g);
+      if(sum1>sum2){
+      System.out.print(true);
+      }
+      else{
+        System.out.print(false);
+      }
+    }
