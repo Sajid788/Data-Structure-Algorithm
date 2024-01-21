4ba36d0b7c122b325e6b41825d74014f19179b9b
Md Sajid Ansari <129252454+Sajid788@users.noreply.github.com>
2024-01-21 19:32:29 +0530

    Create The Ashes! (But One day).java

---
+++ Unit - 1/Conditional Statements and Logical Operator/The Ashes! (But One day).java
@@ -0,0 +1,13 @@
+public static void theAshes(int Australia,int England){
+   if(Australia>England){
+       System.out.println("Australia");
+   }
+   else if(Australia<England)
+   {
+     System.out.println("England");  
+   }
+   else if(Australia==England)
+   {
+      System.out.println("Tie");    
+   }
+  }
